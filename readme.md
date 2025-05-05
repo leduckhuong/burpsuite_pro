1a4a5af7df7a813c238a342e6eebd9918d0be861
leduckhuong <leduckhuong@kali.kalilinux.org>
2025-05-05 09:54:28 +0700

    done

---
+++ readme.md
@@ -0,0 +1,1 @@
+Run script with root shell
--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Kiểm tra nếu không phải đang trong môi trường root shell (sudo su)
+# Kiểm tra có phải root không và không phải đang dùng sudo
 if [[ $(id -un) != "root" || -n "$SUDO_USER" ]]; then
     echo "[!] Vui lòng chạy script từ môi trường root shell (dùng 'sudo su' rồi chạy script)"
     exit 1
 fi
 
-echo "[*] Bạn đang trong root shell. Tiếp tục chạy script..."
+echo "[*] Bạn đang ở root shell, tiếp tục chạy script..."
